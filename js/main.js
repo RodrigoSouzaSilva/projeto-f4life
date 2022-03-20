5b381c8a43d63a022e28ba3102b8151cb3dd6576
rodrigosouzasilva <rodrigo_souza.91@hotmail.com>
2022-03-20 19:28:22 -0300

    atualizando javascript

--- js/main.js
+++ js/main.js
@@ -1,63 +1,95 @@
-let btnEnviar = document.getElementById('btn-enviar');
 let formulario = document.getElementById('formulario');
-let formEmail = document.getElementById('email');
-let formMensagem = document.getElementById('mensagem');
-let confirmacao = document.getElementById('confirmacao');
-let btnConfirmacao = document.getElementById('btn-confirmacao');
+let btnEnviar = document.getElementById('btn-enviar');
+let emailForm = document.getElementById('email');
+let mensagemForm = document.getElementById('mensagem');
+
+let informacao = document.getElementById('informacao');
+let btnInformacao = document.getElementById('btn-informacao');
 
 formulario.addEventListener('submit', e => {
-    e.preventDefault();
-    let email = formEmail.value;
-    let mensagem = formMensagem.value;
+    e.preventDefault(); // cancelando o envio do formulário
 
-    let emaildivido = email.split('@'); // divide pelo @
-    let user = emaildivido[0];          // pega o usuario
-    let domainCom = emaildivido[1];    // pega o dominio.com
+    let email = emailForm.value;
+    let mensagem = mensagemForm.value;
 
-
-
-
-    let domain;
-    if(domainCom == undefined) {
-        domain = false;
-    } else {
-        domain = domainCom.slice(0,-4);  // retira o .com
-    }
-
-
-    console.log(`usuario - ${user} | dominio - ${domain} | msg - ${mensagem}`);
-
-    if(mensagem == false) {
-        confirmacao.innerHTML = `Erro no envio: Insira uma mensagem`;
-        confirmacao.classList.remove('esconde');
-        btnConfirmacao.classList.remove('esconde');
-
-        confirmacao.classList.add('atencao');
-        confirmacao.classList.remove('certo');
-        confirmacao.classList.remove('errado');
-    } else if(user.length <= 32 && domain.length <= 16) {
-        confirmacao.innerHTML = `Obrigado pelo contato, ${user}!`;
-        confirmacao.classList.remove('esconde');
-        btnConfirmacao.classList.remove('esconde');
-
-        confirmacao.classList.add('certo');
-        confirmacao.classList.remove('atencao');
-        confirmacao.classList.remove('errado');
-    } else {
-        confirmacao.innerHTML = `Erro no envio: Endereço de mail inválido`;
-        confirmacao.classList.remove('esconde');
-        btnConfirmacao.classList.remove('esconde');
-
-        confirmacao.classList.add('errado');
-        confirmacao.classList.remove('certo');
-        confirmacao.classList.remove('atencao')
-    }
+    let emailVerificado = verificaEmail(email);
+    let mensagemVerificado = verificaMensagem(mensagem);
+    verificador(emailVerificado, mensagemVerificado);
 })
 
-
-//                       rodrigo@hotmail.com
-
-function apagar() {
-    confirmacao.classList.add('esconde');
-    btnConfirmacao.classList.add('esconde');
+function verificaEmail(email) {   // verifica se email está completo
+    let ultimoCampo = email.length; // pega o ultimo valor do email
+    if(email.indexOf('.com', ultimoCampo - 4) !== -1) {  // email certo, verifica se os 4 ultimos campos são .com
+        let user;
+        let domain;
+        separaEmail(email);
+        return true;
+    } else {
+        return false;
+    }
 }
+
+function verificaMensagem(mensagem) { // verificador de mensagem
+    if(mensagem === '') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function separaEmail(email) { // separa o email em user e domain
+    let emailSeparado = email.split('@');
+    user = emailSeparado[0];
+    domain = emailSeparado[1].slice(0, -4);
+}
+
+function validaEmail(user, domain) { // validador de email
+    if(user.length <= 32 && domain.length <= 16 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function verificador(email, mensagem) {  // verifica se está tudo certo e mostra msg
+    if(email === true && mensagem === true) {
+        mostraCampo(`Obrigado pelo contato, ${user}`, 'certo');
+    }  else if (mensagem !== true) {
+        mostraCampo('Insira uma mensagem','atencao');
+    } else {
+        mostraCampo('Endereço de email inválido', 'errado');
+    }
+}
+
+function mostraCampo(mensagem, classe) { // abre o campo info
+    mostraMensagem(mensagem, classe);
+    mostraBtn();
+}
+
+function mostraMensagem(mensagem, classe) {  // mostra msg no campo info
+    informacao.innerHTML = `${mensagem}`;
+    informacao.classList.add(`${classe}`);
+    informacao.classList.remove('esconde');
+    excluiClasse(classe);   // exclui as classes adicionadas
+}
+
+function mostraBtn() { // mostra o btn de fechar
+    btnInformacao.classList.remove('esconde');
+}
+
+function esconde() { // esconde o btn de fechar e campo informação
+    informacao.classList.add('esconde');
+    btnInformacao.classList.add('esconde');
+}
+
+function excluiClasse(classe) { // função que exclui as classes
+    let classes = ['certo', 'atencao', 'errado'];
+    for(let i = 0; i < classes.length; i++) {
+        if(classes[i] === classe) {
+            continue;
+        } else {
+            informacao.classList.remove(`${classes[i]}`);
+        }
+    }
+}
+
